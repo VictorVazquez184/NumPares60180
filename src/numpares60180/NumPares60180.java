146a29963f9d4368412a441d9b68bde0bff1c26d
RORLe <RORLe@DESKTOP-QL5DQK0>
2021-02-24 20:52:17 -0600

    Los primeros 47 numeros pares

---
+++ src/numpares60180/NumPares60180.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares60180;
+
+/**
+ *
+ * @author RORLe
+ */
+public class NumPares60180 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ int num=0;
+ while(num <= 94){
+  if(num%2==0){
+   System.out.println(num);
+  }
+  num++;
+ }
+}
+}
